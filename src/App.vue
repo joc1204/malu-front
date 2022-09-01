1554a050346e71f99cbbeadedf3b98f5099d8a30
joc.1204 <joc.1204@gmail.com>
2022-09-01 11:34:18 -0300

    Primer commit del front

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import ArticuloDetalle from "./components/ArticuloDetalle.vue";
+
+const articulos = ref([]);
+
+
+//fetch('https://jsonplaceholder.typicode.com/posts/',{
+fetch("http://localhost:3001/articulos"
+//,{mode:"cors"}
+)
+    .then(res => res.json())
+    .then(data => articulos.value = data)
+    .then(console.log(articulos.value))
+    .catch(e=>console.log(e))
+
+</script>
+
+<template>
+    <div class="container">
+
+        <h1>API a Vue</h1>
+        <ArticuloDetalle 
+        v-for="articulo in articulos" 
+        :key="articulo.id" 
+        :title="articulo.codigo"
+        :body="articulo.descripcion"
+        />
+    </div>
+</template>
---
+++ src/components/ArticuloDetalle.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps(['title','body'])
+
+</script>
+
+
+<template>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">{{title}}</h5>
+        
+        <p>{{body}}</p>
+    </div>
+
+</div>
+</template>
+
+<style>
+    h5{
+        color: blue;
+    }
+</style>
